5349e3960e31998769ae82f5752e6198493ec36c
zhimibuwu <405347545@qq.com>
2016-10-07 19:13:31 -0400

    Set up firebase and work on UI using React

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var rootUrl = 'https://react-6a4f2.firebaseio.com/';
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+var App = React.createClass({
+    mixins: [ ReactFire ],
+    componentWillMount: function() {
+        this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    },
+    render: function() {
+        return <div className="row panel panel-default">
+            <div className="col-md-8 col-md-offset-2">
+                <h2 className="text-center">
+                    To-Do List
+                </h2>
+                <Header />
+            </div>
+        </div>
+    }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
