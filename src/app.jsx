c8140b8ce39bcb6483fdca877be57bfdf1c9af60
zhimibuwu <405347545@qq.com>
2016-10-08 13:24:01 -0400

    set up project

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      {this.props.children}
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+var Child1 = React.createClass({
+    render: function() {
+        return <h1>I'm child1</h1>
+    }
+});
+
+var Child2 = React.createClass({
+    render: function() {
+        return <h1>I'm child2</h1>
+    }
+});
+
+var routes = {
+    <Router>
+        <Route path="/" component={Hello}>
+            <Route path="1" component={Child1} />
+            <Route path="2" component={Child2} />
+        </Route>
+    </Router>
+};
+
+ReactDOM.render(routes, document.querySelector('.container'));
