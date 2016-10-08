ea14c4184971286ccbd07be9c6c5312c847c989a
zhimibuwu <405347545@qq.com>
2016-10-07 23:09:38 -0400

    Revert

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactFire = require('reactfire');
-var Firebase = require('firebase');
-var Header = require('./header');
-var rootUrl = 'https://react-6a4f2.firebaseio.com/';
 
-var App = React.createClass({
-    mixins: [ ReactFire ],
-    componentWillMount: function() {
-        this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
-    },
-    render: function() {
-        return <div className="row panel panel-default">
-            <div className="col-md-8 col-md-offset-2">
-                <h2 className="text-center">
-                    To-Do List
-                </h2>
-                <Header />
-            </div>
-        </div>
-    }
+var Hello = React.createClass({
+  render: function() {
+    return <h1 className="red">
+      Hello!
+    </h1>
+  }
 });
 
-var element = React.createElement(App, {});
+var element = React.createElement(Hello, {});
 ReactDOM.render(element, document.querySelector('.container'));
