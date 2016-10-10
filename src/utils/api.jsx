95cd705c84341b2e5788738aa0cb039898b949cf
zhimibuwu <405347545@qq.com>
2016-10-09 21:52:58 -0400

    Functionality improvement: seperate video and image

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,16 +1,16 @@
 var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
-var apiKey = '92344f4ca557402';
+var apiKey = '430d6820d865788';
 
 module.exports = {
-    get: function(url) {
-        return fetch(rootUrl + url, {
-            headers: {
-                'Authorization': 'Client-ID' + apiKey
-            }
-        })
-        .then(function(response){
-            return response.json();
-        })
-    }
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID ' + apiKey
+      }
+    })
+    .then(function(response){
+      return response.json()
+    })
+  }
 };
