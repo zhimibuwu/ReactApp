78aec01d93c26d20aad2fa5dd865211c27a23a4a
zhimibuwu <405347545@qq.com>
2016-10-15 16:15:28 -0400

    Functionality on comment

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -30,7 +30,7 @@ module.exports = React.createClass({
         </nav>
     },
     renderTopics: function() {
-        return this.state.topics.slice(0,4).map(function(topic){
+        return this.state.topics.map(function(topic){
             return <li key={topic.id}>
                 <Link activeClassName="active" to={"topics/" + topic.id}>
                     {topic.name}
